d7eed1bfbcdf0ab67f8e6cc308cf2e927afa827a
Grzegorz Pokorski <mr.pokorski@gmail.com>
2024-02-20 11:57:05 +0100

    wip

---
+++ .lintstagedrc.js
@@ -0,0 +1,3 @@
+module.exports = {
+  "*.{js,jsx,ts,tsx,md,mdx,graphql,yml,yaml,css,scss,json}": `prettier --write --ignore-unknown`,
+};
--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,4 +1,4 @@
-import "@/styles/global";
+import "@/styles/e";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 
